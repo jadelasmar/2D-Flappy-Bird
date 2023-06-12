4a846d37054e1d11f8fb2a3dff9af687ea56df32
elBrøwn <elbrown.jad@gmail.com>
2023-06-12 14:12:22 +0300

    one pipe only

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -35,49 +35,16 @@ public class GameManager : MonoBehaviour
             
         switch (difficulty)
         {
-            case 1:
-                speed = 2;
-                break;
-
-            case 2:
-                break;
-                
-            case 3:
-                AddObstacle();
-                break;
-
-            case 4:
-                speed = 3;
-                break;
-
-            case 5:
-                break;
-                
-            case 6:
-                AddObstacle();
-                break;
-
-            case 7:
-                speed = 4;
-                break;
-                
             default:
                 speed++;
                 break;
+            
+            case 2:
+            case 5:
+                break;
         }
     }
 
-    private void AddObstacle()
-    {
-        for (var i = 0; i < pipes.childCount; i++)
-        {
-            if (pipes.GetChild(i).gameObject.activeSelf) continue;
-            pipes.GetChild(i).gameObject.SetActive(true);
-            initialPipe.position = new Vector2(initialPipe.position.x+10, initialPipe.position.y);
-            break;
-
-        }
-    }
 
     public static void GameOver()
     {
